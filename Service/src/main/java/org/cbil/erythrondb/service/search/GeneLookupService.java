52da7dc6f294308745543d0c3fd153fc5f39f736
fossilfriend <egreenfest@gmail.com>
2020-08-14 14:27:31 -0400

    gene lookup service

---
+++ Service/src/main/java/org/cbil/erythrondb/service/search/GeneLookupService.java
@@ -0,0 +1,73 @@
+package org.cbil.erythrondb.service.search;
+
+import static org.gusdb.fgputil.FormatUtil.NL;
+
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import org.apache.log4j.Logger;
+import org.gusdb.fgputil.db.runner.BasicResultSetHandler;
+import org.gusdb.fgputil.db.runner.SQLRunner;
+import org.gusdb.wdk.model.WdkModel;
+import org.gusdb.wdk.model.WdkModelException;
+import org.gusdb.wdk.model.WdkRuntimeException;
+import org.gusdb.wdk.service.service.AbstractWdkService;
+
+@Path("lookup/gene")
+public class GeneLookupService extends AbstractWdkService {
+    private static final Logger LOG = Logger.getLogger(GeneLookupService.class);
+    private static final String SEARCH_TERM_PARAM = "id";
+    
+    private static final String SEARCH_QUERY =  "SELECT jsonb_agg(DISTINCT" + NL
+    + "jsonb_build_object('source_id', ga.source_id," + NL 
+    + "'chromosome', ga.chromosome," + NL 
+    + "'start', ga.start_min, 'end', ga.end_max," + NL 
+    + "'symbol', ga.gene_symbol, 'organism'," + NL
+    + "ga.organism, 'product', annotation->>'name'))" + NL
+    + "FROM CBIL.GeneIdentifiers gi, CBIL.GeneAttributes ga" + NL
+    + "WHERE external_id ILIKE '%' || ? || '%'" + NL
+    + "AND gi.gene_id = ga.gene_id";
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    // @OutSchema("niagads.search.sitesearch.get-response")
+    public Response buildResponse(String body, @QueryParam(SEARCH_TERM_PARAM) String term) throws WdkModelException {
+        LOG.info("Starting 'GeneLookup' Service");
+        String response = "{}";
+        try {
+            response = lookupTerm(term);
+            if (response == null) { response = "[]";}
+            LOG.debug("query result: " + response);
+        }
+        
+        catch(WdkRuntimeException ex) {
+            throw new WdkModelException(ex);
+        }
+        
+        return Response.ok(response).build();
+    }
+    
+    private String lookupTerm(String term) {   
+        
+        WdkModel wdkModel = getWdkModel();
+        DataSource ds = wdkModel.getAppDb().getDataSource();
+        BasicResultSetHandler handler = new BasicResultSetHandler();
+        
+        LOG.debug("Looking up term for site search: " + term);
+        LOG.debug("SiteSearch Query: " + SEARCH_QUERY);
+        SQLRunner runner = new SQLRunner(ds, SEARCH_QUERY, "site-search-query");
+        runner.executeQuery(new Object[] {term}, handler);
+        
+        List <Map <String, Object>> results = handler.getResults();
+        return (String) results.get(0).get("result");
+    }
+    
+}
