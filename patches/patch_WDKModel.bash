d2cf143c17f3757e63126c105e5f57ba280ddb51
fossilfriend <egreenfest@gmail.com>
2022-11-15 10:45:30 -0500

    apply patches to correct repos

--- patches/patch_WDKModel.bash
+++
@@ -1,2 +0,0 @@
-# temp patch to fix datasetParams; should be patched in build-48
-# cp $PROJECT_HOME/ErythronDBWebsite/patches/DatasetFactory.java $PROJECT_HOME/WDK/Model/src/main/java/org/gusdb/wdk/model/dataset/DatasetFactory.java
